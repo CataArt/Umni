cb20b344ffb545f1b4debb5352c84ee5561051a7
CataArt <none@no.ne>
2024-01-07 13:59:46 +0900

    変換するTSを追加したつもり

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 "use strict";
 alert("1");
+function submit() {
+    const editor = document.getElementById('editor');
+    if (editor) {
+        editor.value = editor.value.replace(/あ/g, 'い');
+    }
+}
