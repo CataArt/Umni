026e0ede8f0c2308a4f0f3649e4787c7c121f9d9
CataArt <none@no.ne>
2024-01-07 14:00:15 +0900

    Updates

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
 alert("1");
+
+function submit(): void {
+  const editor = document.getElementById('editor') as HTMLTextAreaElement;
+  if (editor) {
+    editor.value = editor.value.replace(/あ/g, 'い');
+  }
+}
