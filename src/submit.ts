59671595eb3686d77f4ec3a8e322ef2402c43f99
CataArt <none@no.ne>
2024-01-19 22:34:53 +0900

    いかころ拡張と新テク追加

--- src/submit.ts
+++ src/submit.ts
@@ -19,9 +19,22 @@ async function submit(): Promise<void> {
     const sortedReplacements = Array.from(replacements)
       .sort((a, b) => b[0].length - a[0].length);
 
+      sortedReplacements.forEach(([original, replacement]) => {
+        if (replacement.length >= 14) {
+          // 変換後の文字列が5文字以上の場合、最初のマッチのみ置換
+          value = value.replace(new RegExp(original), replacement.replace(/[\n\r]+/, '').replace(/\\n/g, '\n'));
+        } else {
+          // それ以外の場合は全てのマッチを置換
+          value = value.replace(new RegExp(original, 'g'), replacement.replace(/[\n\r]+/, '').replace(/\\n/g, '\n'));
+        }
+      });
+      value = value.replace(/底底/g, '底'));
+
+      /*
     sortedReplacements.forEach(([original, replacement]) => {
-      value = value.replace(new RegExp(original, 'g'), replacement.replace(/[\n\r]+/, ''));
+      value = value.replace(new RegExp(original, 'g'), replacement.replace(/[\n\r]+/, '').replace(/\\n/g, '\n'));
     });
+    */
     editor.value = value;
   }
 }
