f04bfd5cb0e15164a94a274a84719e9c27a832c8
CataArt <none@no.ne>
2024-02-02 21:14:06 +0900

    Updates

--- src/submit.ts
+++ src/submit.ts
@@ -20,7 +20,7 @@ async function submit(): Promise<void> {
       .sort((a, b) => b[0].length - a[0].length);
 
       sortedReplacements.forEach(([original, replacement]) => {
-        if (replacement.length >= 9) {
+        if (replacement.length >= 8) {
           // 変換後の文字列が5文字以上の場合、最初のマッチのみ置換
           value = value.replace(new RegExp(original), replacement.replace(/[\n\r]+/, '').replace(/\\n/g, '\n'));
         } else {
