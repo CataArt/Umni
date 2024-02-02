fcd0366eecfb128f762093a29c649b060e9f91d0
CataArt <none@no.ne>
2024-02-02 23:02:12 +0900

    Updates

--- src/submit.ts
+++ src/submit.ts
@@ -11,7 +11,7 @@ async function submit(): Promise<void> {
     if (checkbox.checked) {
       const dictReplacements = await loadReplacements(checkbox.value);
       dictReplacements.forEach((val, key) => {
-        const tempKey = `[[${counter++}_${'a'.repeat(val.length)}]]`; // 一時的なキーを生成
+        let tempKey = `[[${counter++}_${'a'.repeat(val.length)}]]`; // 一時的なキーを生成
         replacements.set(tempKey, val);
         tempReplacements.set(key, tempKey); // 元のキーを一時的なキーにマッピング
       });    
@@ -26,7 +26,7 @@ async function submit(): Promise<void> {
       .sort((a, b) => b[0].length - a[0].length);
 
     tempSortedReplacements.forEach(([original, replacement]) => {
-      if (replacement.length >= 12) {
+      if (replacement.length >= 13) {
         // 変換後の文字列が8文字以上の場合、最初のマッチのみ置換
         value = value.replace(new RegExp(original), replacement.replace(/[\n\r]+/, '').replace(/\\n/g, '\n'));
       } else {
