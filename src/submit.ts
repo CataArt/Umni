15a220a76ce180b452159c1bf2820d0efe9e9cf9
CataArt <none@no.ne>
2024-02-02 23:55:11 +0900

    Updates

--- src/submit.ts
+++ src/submit.ts
@@ -11,15 +11,15 @@ async function submit(): Promise<void> {
     if (checkbox.checked) {
       const dictReplacements = await loadReplacements(checkbox.value);
       dictReplacements.forEach((val, key) => {
-        let tempKey = `[[${counter++}_${'a'.repeat(val.length)}]]`; // 一時的なキーを生成
+        let tempKey = `★★${counter++}_${'a'.repeat(val.length)}★★`; // 一時的なキーを生成
         replacements.set(tempKey, val);
         tempReplacements.set(key, tempKey); // 元のキーを一時的なキーにマッピング
       });    
     }
   }
 
-  console.log(replacements);
-  console.log(tempReplacements);
+  //console.log(replacements);
+  //console.log(tempReplacements);
 
   if (editor) {
     let value = editor.value;
@@ -29,9 +29,9 @@ async function submit(): Promise<void> {
       .sort((a, b) => b[0].length - a[0].length);
 
     tempSortedReplacements.forEach(([original, replacement]) => {
-      console.log("tempforeach")
-      console.log(original);
-      console.log(replacement);
+      //console.log("tempforeach")
+      //console.log(original);
+      //console.log(replacement);
       if (replacement.length >= 13) {
         // 変換後の文字列が8文字以上の場合、最初のマッチのみ置換
         value = value.replace(new RegExp(original), replacement.replace(/[\n\r]+/, '').replace(/\\n/g, '\n'));
