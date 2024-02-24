ee1ecac738412507da1a8cba093797ffe65e3f26
CataArt <none@no.ne>
2024-02-25 00:33:38 +0900

    元の文章を表示するようにした

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const checkboxes = document.querySelectorAll('input[type="checkbox"][name="dictionary"]') as NodeListOf<HTMLInputElement>;
   const translateButton = document.getElementById('translateButton') as HTMLButtonElement | null;
   const replaceProbabilityInput = document.getElementById('replace_probability') as HTMLInputElement | null;
-  if (editor && translateButton && replaceProbabilityInput) {
+  const originalTextArea = document.getElementById('original_text_area') as HTMLTextAreaElement | null;
+  if (editor && translateButton && replaceProbabilityInput && originalTextArea) {
     translateButton.addEventListener('click', async () => {
+      // 元のテキストを表示
+      originalTextArea.value = editor.value;
+
       // チェックされた辞書のvalueを集めた配列を作成
       const checkedDictionaries = Array.from(checkboxes)
         .filter(checkbox => checkbox.checked)
