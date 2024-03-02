2b556e9ddfddaac106a2099d782f2909d2c9967a
CataArt <none@no.ne>
2024-03-02 19:13:08 +0900

    translate.ts のテストをカバレッジ100%にした

--- test/translate.test.ts
+++ test/translate.test.ts
@@ -33,6 +33,29 @@ describe('辞書を選択する機能', () => {
   });
 });
 
+describe('置換元が長い順に優先して置換されるテスト', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('長い置換元が短い置換元よりも優先されて置換されること', async () => {
+    const replacementsMap = new Map([
+      ['short', 'S'],
+      ['longes', 'L'],
+      ['longest', 'LG'],
+    ]);
+    const originalText = 'longest short longes longest';
+    const expectedText = 'LG S L LG';
+    jest.spyOn(global.Math, 'random').mockReturnValue(0); // 100%の確率で置換が行われるように設定
+
+    (loadReplacements as jest.Mock).mockResolvedValue(replacementsMap);
+
+    const translatedText = await translate(originalText, ['dict'], '100');
+
+    expect(translatedText).toBe(expectedText);
+  });
+});
+
 describe('置換先の長さによる挙動のテスト', () => {
   beforeEach(() => {
     jest.clearAllMocks();
@@ -40,15 +63,16 @@ describe('置換先の長さによる挙動のテスト', () => {
   });
 
   it.each([
-    ['短い置換先', new Map([['short', 'shortRep']]), 'shortText', 'shortRepText', 100],
+    ['短い置換先', new Map([['short', 'short2']]), 'shortText', 'short2Text', 100],
     ['長い置換先', new Map([['long', 'longReplacementThatIsVeryLong']]), 'longText', 'longReplacementThatIsVeryLongText', 100],
-    ['長い置換先で置換なし', new Map([['long', 'longReplacementThatIsVeryLong']]), 'longText', 'longText', 1], // Math.random()が0を返す場合、置換されない
+    // randomValue の受け付ける範囲が 1-100 なのに気をつける (本当は0の時に例外を出すなりするのが良い)
+    ['短い置換先で置換なし', new Map([['short', 'short2']]), 'shortText', 'shortText', 1],
+    ['長い置換先で置換なし', new Map([['long', 'longReplacementThatIsVeryLong']]), 'longText', 'longText', 1],
   ])('%s', async (description, replacementsMap, originalText, expectedText, randomValue) => {
     jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
     (loadReplacements as jest.Mock).mockResolvedValue(replacementsMap);
 
     const translatedText = await translate(originalText, ['dict'], randomValue.toString());
-
     expect(translatedText).toBe(expectedText);
   });
 });
