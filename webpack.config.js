a613b7d21d8548488c2e9959be600a8f9eed6b05
CataArt <none@no.ne>
2024-04-14 21:02:30 +0900

    github pagesでの公開をpublic/だけにした

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.ts',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public/dist'),
   },
   module: {
     rules: [
@@ -18,5 +19,19 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.js'],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      terserOptions: {
+        compress: {
+          drop_console: false, // コンソールログを削除しない
+        },
+        mangle: {
+          properties: false, // プロパティ名は短縮しない（注意が必要）
+          toplevel: true, // トップレベルの変数と関数名も短縮
+        },
+      },
+    })],
   },
 };
